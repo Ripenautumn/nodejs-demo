62c197f23bff2a2d405b7789d4e96ce54f18fcc1
wangding <408542507@qq.com>
2020-09-18 20:01:27 +0800

    modify demo code

--- 08-event/02-dog.js
+++ 08-event/02-dog.js
@@ -1,26 +1,22 @@
-#!/usr/bin/node
-
 const EventEmitter = require('events').EventEmitter;
 
 function Dog(name, energy) {
-  var _name   = name,
+  let _name   = name,
       _energy = energy,
       that    = this;
 
   EventEmitter.call(this);
 
-  var timer = setInterval(() => {
-    if(energy > 0) {
+  let timer = setInterval(() => {
+    if(_energy > 0) {
       that.emit('bark');
       _energy--;
-    }
-
-    if(_energy < 0) {
+    } else {
       clearInterval(timer);
     }
   }, 1000);
 
-  this.name = () => _name;
+  this.name   = () => _name;
   this.energy = () => _energy;
 }
 
